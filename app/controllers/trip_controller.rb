1685a10772141bea610c218b9a52b2b075d046ca
Chris Metcalf <krezel@carfrax.lan>
2009-09-15 23:23:29 -0700

    Basic functionality is working now, stubbed out some other parts

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -7,16 +7,27 @@ class TripController < ApplicationController
 
   def show
     @trip = Trip.find_by_id(params[:id])
+    @title = @trip.name
+
     @pings = @trip.pings.all(:order => :when_timestamp)
 
     @map = GMap.new("map")
     @map.control_init(:large_map => true, :map_type => true)
 
     # Create our initial point
-    marker = GMarker.new([@pings.first.latitude, @pings.first.longitude],
-                         :title => @pings.first.when_timestamp.to_s,
-                         :info_window => @pings.first.message)
+    @latest = @pings.first
+    marker = GMarker.new([@latest.latitude, @latest.longitude],
+                         :title => @latest.when_timestamp.to_s,
+                         :info_window => "<h3>#{@latest.when_timestamp}:</h3> #{@latest.message}")
     @map.overlay_init(marker)
+
+    # Add the rest of the pings
+    @pings.each do |p|
+      @map.record_init @map.add_overlay(GMarker.new([p.latitude, p.longitude],
+                                                    :title => p.when_timestamp.to_s,
+                                                    :info_window => 
+                                                      "<h3>#{p.when_timestamp}:</h3> #{p.message}"))
+    end
 
     @map.center_zoom_init([@pings.first.latitude, @pings.first.longitude], 4)
   end
